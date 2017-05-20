90bcd112adbc03d19c3db916feba61de54b3cc90
Isabel Ramos <iramos1226@gmail.com>
2017-05-20 10:49:35 -0500

    divided info into two scope variables

--- javascripts/controllers/GoingToTheSunCtrl.js
+++ javascripts/controllers/GoingToTheSunCtrl.js
@@ -1,11 +1,17 @@
-app.controller("GoingToTheSunCtrl", function($http, $location, $q, $scope) {
-	$scope.goingToTheSunInfo = [
+app.controller("GoingToTheSunCtrl", function($http, $location, $scope) {
+	$scope.goingToTheSunName = [
         {
           name: "Going to the Sun Road",
-          photo: "https://www.nps.gov/glac/planyourvisit/images/gtsrblend.jpg",
+          photo: "https://www.nps.gov/glac/planyourvisit/images/gtsrblend.jpg"
+      	}
+    ];
+
+    $scope.goingToTheSunDescription = [
+        {
           description: "Built in the Roaring 1920s to tempt drivers to explore the national parks, Going-to-the-Sun Road is a breathtaking 50-mile drive across Montana’s Glacier National Park. Rising up between a pair of deep-blue alpine lakes, the road was cleverly crafted to show off the best vistas with the least negative impact on the park’s fragile environment. It’s closed every winter by snows, which can reach up to 80 feet in depth."
       	}
-      ];
+    ];
+
 
 
 
