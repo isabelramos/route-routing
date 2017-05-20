f8b8f3462c2d3e24991f436bbb9e3c97d93e54de
Isabel Ramos <iramos1226@gmail.com>
2017-05-20 13:52:44 -0500

    simplified code

--- javascripts/controllers/GoingToTheSunCtrl.js
+++ javascripts/controllers/GoingToTheSunCtrl.js
@@ -6,11 +6,7 @@ app.controller("GoingToTheSunCtrl", function($http, $location, $scope) {
       	}
     ];
 
-    $scope.goingToTheSunDescription = [
-        {
-          description: "Built in the Roaring 1920s to tempt drivers to explore the national parks, Going-to-the-Sun Road is a breathtaking 50-mile drive across Montana’s Glacier National Park. Rising up between a pair of deep-blue alpine lakes, the road was cleverly crafted to show off the best vistas with the least negative impact on the park’s fragile environment. It’s closed every winter by snows, which can reach up to 80 feet in depth."
-      	}
-    ];
+    $scope.goingToTheSunDescription = "Built in the Roaring 1920s to tempt drivers to explore the national parks, Going-to-the-Sun Road is a breathtaking 50-mile drive across Montana’s Glacier National Park. Rising up between a pair of deep-blue alpine lakes, the road was cleverly crafted to show off the best vistas with the least negative impact on the park’s fragile environment. It’s closed every winter by snows, which can reach up to 80 feet in depth.";
 
 
 
